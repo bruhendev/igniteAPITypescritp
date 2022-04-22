0d9d10c08d1ae9105b5df30569a857994f58ea40
Bruno <brunohenrique242@gmail.com>
2022-04-22 18:53:54 -0300

    fim do capitulo 1

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    return res.json({message: "Hello World"});
-})
+    return res.json({ message: "Hello World" });
+});
+
+app.use("/categories", categoriesRoutes);
+
 
 app.listen(3333, () => console.log('server running port 3333'));
 
